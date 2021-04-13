e4fee32a0bb5e4cb79ca9fbc815b883afdff3d93
Mansa Samlafo <msamlafo@gmail.com>
2021-04-13 01:39:16 -0400

    updated db file

--- db.js
+++ db.js
@@ -5,9 +5,21 @@ const Sequelize = require('sequelize');
 // const database = new Sequelize (process.env.NAME, 'postgres', process.env.PASS, {host:'localhost',
 // dialect:'postgres'});
 
-const database = new Sequelize (process.env.DATABASE_URL, {
-    dialect:'postgres'});
+// const database = new Sequelize (process.env.DATABASE_URL, {
+//     dialect:'postgres'});
 //authenticate that the username and password match, then log into the database
+
+const database = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: !process.env.DATABASE_URL.includes('postgres')
+      ? {
+          ssl: {
+            rejectUnauthorized: false,
+            require: true,
+          },
+        }
+      : {},
+  });
 
 database.authenticate()
 .then(()=> console.log('postgres db is connected'))
